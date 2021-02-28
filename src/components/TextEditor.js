461b86da870b09c328f56ecd4de3f5828410f23b
Julio Magalhaes <jcmagalhaes301@gmail.com>
2021-02-28 19:44:48 -0300

    criando função para negrito

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,19 +1,74 @@
 import React, { Component } from 'react'
 import { Editor } from 'slate-react'
 import { Value } from 'slate'
+import BoldMark from "./BoldMark";
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes : [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'My first paragraph',
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+})
 
 export default class TextEditor extends Component {
+
   state = {
-    value: '',
+    value: initialValue,
   }
 
   onChange = ({ value }) => {
     this.setState({ value })
   }
 
+  onKeyDown = (e, change) => {
+
+    if (!e.ctrlKey) { return }
+    e.preventDefault()
+
+    switch (e.key) {
+      case 'b': {
+        change.toggleMark('bold')
+        return true
+      }
+    
+      default:
+        break;
+    }
+
+    console.log(e.key)
+  }
+
+  renderMark = props => {
+    switch (props.mark.type) {
+      case 'bold':
+        return <BoldMark {...props} />    
+      default:
+        break;
+    }
+  }
+
   render() {
     return (
-      <Editor value = { this.state.value } onChange = { this.onChange } />
+      <Editor
+        value={this.state.value}
+        onChange={this.onChange}
+        onKeyDown={this.onKeyDown}
+        renderMark={this.renderMark}
+      />
     )
   }
 }
